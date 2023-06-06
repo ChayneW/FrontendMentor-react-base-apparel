cd9f5375a70663b25fddf8e9581a01669cad59a8
Chaynes-MacBookPro <ChayneW-MacBookPro.local>
2023-06-06 19:30:05 -0400

    update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -5,24 +5,8 @@ import Hero from '@/components/Hero'
 const page = () => {
 
   return (
-    // <div className='card'>
-
-    // hi
-    // </div>
         <div className='card'>
-            {/* <div className='logo'>
-              <img src='logo.svg'/>
-            </div> */}
-
             <Hero />
-
-            {/* <div className='img-container'>
-
-              <picture className='img-pic-tag'>
-                <source className='img-pic-tag-imgdesk' srcSet='hero-desktop.jpg' alt='desktop-img' media='(min-width: 380px)'/>
-                <img className='img-pic-tag-imgmbl' src='hero-mobile.jpg' alt='mobile-img'/>
-              </picture>
-            </div> */}
         </div>
 
   )
